0fd2b17b5dd3ad392781c70e3619728c33f47079
tveloso <32099748+tveloso@users.noreply.github.com>
2020-10-17 11:35:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
 var selectedTown;
-const urlMeteo = 'https://www.prevision-meteo.ch/services/json';
+const urlMeteo = 'https://www.prevision-meteo.ch/services/json/';
 
-function getTownWeather(town){
-    if (town === undefined){
+function getTownWeather(town) {
+    if (!town) {
         town = document.querySelector('#town').value;
     }
-    let url = urlMeteo+'/'+town;
+    let url = urlMeteo + town;
     getWeather(url);
 }
 
 function getWeather(url) {
-    fetch(url).then(response =>{
-        response.json().then( 
-             json => { 
-                displayWeather(json)  
-        })
-});}
+    fetchJSONFromURL(url, displayWeather);
+}
 
 function displayWeather(json){
     let weather = new Weather(json);
     weather.display();
 }
+
 var jsonData;
 const mapClass = new Map();
-mapClass.map.on('click', onMapClick);
-function onMapClick(e) {
+mapClass.map.on('click', onClickMap);
+
+function onClickMap(e) {
     mapClass.popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(mapClass.map);
         mapClass.map.setView(e.latlng,8);
-    if(confirm('Show weather ?')){
+    if (confirm('Show weather ?')) {
         gpsPoint = new GPSPoint(e.latlng.lat,e.latlng.lng);
         let url = gpsPoint.getUrlJson();
         console.log(url);
@@ -38,27 +36,30 @@ function onMapClick(e) {
     }
 }
 
-function getTownsVicopo(){
+function getTownsVicopo() {
     selectedTown = document.querySelector('#town').value;
-    let url = 'https://vicopo.selfbuild.fr/?city='+selectedTown;
+    let url = `https://vicopo.selfbuild.fr/?city=${selectedTown}`;
     getCities(url);
 }
 
-function getCities(url){
-    fetch(url).then(response =>{
-        response.json().then( 
-            json => { 
-                displayOptions(json) 
-        })
-    });
+function getCities(url) {
+    fetchJSONFromURL(url, displayOptions);
 }
 
-function displayOptions(json){
+function fetchJSONFromURL(url, callback) {
+    fetch(url)
+        .then(response => response.json())
+        .catch(error => console.error(error))
+        .then(json => { callback(json); })
+        .catch(error => console.error(error));
+}
+
+function displayOptions(json) {
     jsonData = json;
     let div = document.querySelector("#select");
     div.innerHTML = '';
-    if(json.cities[0]){
-        if(json.cities[1] || selectedTown.toLowerCase() != json.cities[0].city.toLowerCase()){
+    if (json.cities[0]) {
+        if (json.cities[1] || selectedTown.toLowerCase() != json.cities[0].city.toLowerCase()) {
             let template = document.querySelector("#townsSelect");
             let clone = document.importNode(template.content, true);
             div.appendChild(clone);
@@ -68,33 +69,32 @@ function displayOptions(json){
             option.text = json.cities[key].code+' '+json.cities[key].city;
             select.add(option);
             }
+            // Supprimer le onchange dans le html du coup
+            select.addEventListener('change', (e) => {
+                onChangeSelectedTownWeather(e);
+            });
         }
     }
-    else{
+    else {
         alert("ville inconnue!");
     }
 }
 
-function getSelectedTownWeather(){
-    let selectTown = document.getElementById("townSelect"); 
-    let town = selectTown.options[selectTown.selectedIndex].value;
-    let cpt = 0;
-    for(let key in jsonData.cities){
-        if (jsonData.cities[key].city === town.substr(6)){
-            cpt++;
-            if(cpt > 1){
-                break;
-            }
-        }
-
+function onChangeSelectedTownWeather(e) {
+    const selectedTown = e.target.value;
+    let filteredCities = [];
+    let town;
+    if (jsonData && Array.isArray(jsonData.cities) && jsonData.cities.length > 0) {
+        filteredCities = jsonData.cities.filter(city => selectedTown.substr(6) === city.city);
+        
+    } else {
+        console.error(`Error : ${jsonData} ${jsonData.cities}`);
     }
-    if (cpt>1){
-        town = town.substr(6)+'-'+town.substr(0,2);
-    }
-    else{
-        town = town.substr(6);
+    
+    if (filteredCities.length > 1) {
+        town = `${selectedTown.substr(6)}-${selectedTown.substr(0,2)}`;
+    } else {
+        town = selectedTown.substr(6);
     }
     getTownWeather(town);
 }
-
-
